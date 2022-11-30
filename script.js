fe2afb651395a300989011921386b0a87e00a399
Le_Vrai_WIWI <superwoup@gmail.com>
2022-11-30 19:41:08 +0100

    physique

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// variables
 let tabJeux = [];
 let joueur = 1;
-
 let ligne = prompt("combien de colones ?");
 let row = ligne++;
 
+// création du tableau
 let table = document.createElement("table");
 for (let i = 0; i < row; i++) {
   tabJeux.push([]);
@@ -15,15 +16,35 @@ for (let i = 0; i < row; i++) {
 }
 document.body.appendChild(table);
 
+// physique du jeux
 let tr = document.getElementsByTagName("tr");
 for (let i = 0; i < tr.length; i++) {
   tr[i].addEventListener("click", function () {
     for (let j = 0; j < tabJeux[i].length; j++) {
       if (tabJeux[i][j] === 0) {
         tabJeux[i][j] = joueur;
+        if (joueur === 1) {
+          joueur = 2;
+        } else {
+          joueur = 1;
+        }
         break;
       }
     }
+    updateDom();
     console.log(tabJeux);
   });
+}
+
+// fonction qui met à jour le dom
+function updateDom() {
+  for (let i = 0; i < tr.length; i++) {
+    for (let j = 0; j < tabJeux[i].length; j++) {
+      if (tabJeux[i][j] === 1) {
+        tr[i].children[j].style.backgroundColor = "red";
+      } else if (tabJeux[i][j] === 2) {
+        tr[i].children[j].style.backgroundColor = "yellow";
+      }
+    }
+  }
 }
