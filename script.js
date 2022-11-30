2f23e27990900cb7b5a2fac2eb30b23c5994e062
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2022-11-30 18:45:52 +0100

    pushhhh

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let tabJeux = [];
+let joueur = 1;
 
 row = prompt("combien de colones ?");
 ligne = row++;
@@ -13,6 +14,26 @@ for (let i = 0; i < row; i++) {
   }
 }
 document.body.appendChild(table);
+
+// rendre les colloones cliquable en renvoyant leur id du tabJeux
+let td = document.getElementsByTagName("td");
+for (let i = 0; i < td.length; i++) {
+  td[i].addEventListener("click", function () {
+    let id = this.id;
+    let idSplit = id.split("-");
+    let idRow = idSplit[0];
+    let idCol = idSplit[1];
+    console.log(idRow, idCol);
+    if (joueur === 1) {
+      this.style.backgroundColor = "red";
+      joueur = 2;
+    } else {
+      this.style.backgroundColor = "blue";
+      joueur = 1;
+    }
+  });
+}
+// Ne pas prendre en compte
 
 console.log(tabJeux);
 console.log(tabJeux.length);
