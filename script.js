f06e1ff50a6f760c70d69cd9a8ea36956e872f28
Le_Vrai_WIWI <superwoup@gmail.com>
2022-11-30 15:46:13 +0100

    p4

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-// row = prompt("combien de colones ?");
-// ligne = prompt("combien de lignes ?");
-joueur1 = prompt("nom du joueur 1");
-joueur2 = prompt("nom du joueur 2");
+let tabJeux = [];
 
-row = 5;
-ligne = 5;
+row = prompt("combien de colones ?");
+ligne = prompt("combien de lignes ?");
+// joueur1 = prompt("nom du joueur 1");
+// joueur2 = prompt("nom du joueur 2");
 
 let table = document.createElement("table");
 for (let i = 0; i < row; i++) {
   let tr = table.appendChild(document.createElement("tr"));
-  console.log("on cree un tr");
+  tabJeux.push([]);
   for (let j = 0; j < ligne; j++) {
     let td = tr.appendChild(document.createElement("td"));
-    console.log("on cree un td");
   }
 }
-document.td.innerHTML = "fleur";
+document.body.appendChild(table);
+console.log(tabJeux);
 
-// Etape 1
-// On crée l'élément HTML vide de contenu
-// En fait on crée ça <h1></h1>
-var els = document.createElement("h1");
-
-// Etape 2
-// Ici on crée un contenu
-var contenu = document.createTextNode("Bonjour à tous");
-
-// Etape 3
-// le contenu et l'élément ne se connaissent pas
-// Il faut ajouter le contenu en tant qu'enfant de l'élément
-els.appendChild(contenu);
-
-// Etape 4
-// Maintenant il faut acrocher la branche dans l'arbre
-// On accède au nœud body avec document.body
-document.body.appendChild(els);
+function remplisLaListe(liste, joueur) {}
