b3beba63a1209e7854b24b3abd2b6041ecbfe64c
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2022-12-01 22:09:50 +0100

    Fonction des vérifications gagnants
    Erreur de out of range

--- script.js
+++ script.js
@@ -85,22 +85,22 @@ function weHaveAWinner() {
       }
     }
   }
-
-  // vérifier verticalement
+  
+  // vérifier horizontale
   for (let i = 0; i < tabJeux.length - 3; i++) {
     for (let j = 0; j < tabJeux[i].length; j++) {
       if (
         tabJeux[i][j] !== 0 &&
-        tabJeux[i][j] === tabJeux[i + 1][j] &&
-        tabJeux[i][j] === tabJeux[i + 2][j] &&
-        tabJeux[i][j] === tabJeux[i + 3][j]
+        tabJeux[i][j] === tabJeux[i][j+1] &&
+        tabJeux[i][j] === tabJeux[i][j+2] &&
+        tabJeux[i][j] === tabJeux[i][j+3]
       ) {
         return true;
       }
     }
   }
-
-  // vérifier en diagonale
+  
+  // vérifier en diagonale BG - HD
   for (let i = 0; i < tabJeux.length - 3; i++) {
     for (let j = 0; j < tabJeux[i].length - 3; j++) {
       if (
@@ -113,6 +113,20 @@ function weHaveAWinner() {
       }
     }
   }
-
+  
+  // vérifier en diagonale BD - HG
+  for (let i = 0; i < tabJeux.length - 3; i++) {
+    for (let j = 0; j < tabJeux[i].length - 3; j++) {
+      if (
+        tabJeux[i][j] !== 0 &&
+        tabJeux[i][j] === tabJeux[i - 1][j + 1] &&
+        tabJeux[i][j] === tabJeux[i - 2][j + 2] &&
+        tabJeux[i][j] === tabJeux[i - 3][j + 3]
+      ) {
+        return true;
+      }
+    }
+  }
+  
   return false;
 }
